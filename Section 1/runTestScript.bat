915501c5f912f36c3c929c4a902623e886fd610f
PacktNishanthS <49745506+PacktNishanthS@users.noreply.github.com>
2019-05-28 12:58:04 +0530

    Delete runTestScript.bat

--- Section 1/runTestScript.bat
+++
@@ -1,6 +0,0 @@
-rem This batch script sets the path to the install directory for R
-rem by default  R is installed in Program Files
-
-set RHOME="C:/Program Files/R/R-3.3.0"
-
-%RHOME%\bin\Rscript.exe "C:\Users\rskeggs\Dropbox\Packt\R\Initial\Section 1\1.4\testScript.r"
